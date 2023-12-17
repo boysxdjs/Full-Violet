90cf38922808cec42dcc59dcd826b327802588d4
xxddbotsx <149536321+boysxdjs@users.noreply.github.com>
2023-12-17 18:03:54 -0500

    𓍢ִ໋💜⊹ 𝗩𝗜𝗢𝗟𝗘𝗧 𝗕𝗢𝗧  𓍢ִ໋💜⊹

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,9 +6,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `${pesan}`;
-  let teks = `𝗩𝗶𝗼𝗹𝗲𝘁 𝗕𝗼𝘁 𝐃𝐢𝐜𝐞 𝐐𝐮𝐞 𝐓𝐞 𝐀𝐜𝐭𝐢𝐯𝐞𝐬 𝐏𝐥𝐚𝐧𝐭𝐢𝐭𝐚 🌱\n${oi}`;
+  let teks = `𝗩𝗶𝗼𝗹𝗲𝘁 𝗕𝗼𝘁 𝐃𝐢𝐜𝐞 𝐐𝐮𝐞 𝐓𝐞 𝐀𝐜𝐭𝐢𝐯𝐞𝐬 𝐏𝐥𝐚𝐧𝐭𝐢𝐭𝐚 🌱\n${oi}\n`;
   for (const mem of participants) {
-    teks += ` 𓍢ִ໋💜⊹┣➥ @${mem.id.split('@')[0]}\n`;
+    teks += ` 𓍢ִ໋💜⊹➢ @${mem.id.split('@')[0]}\n`;
   }
   teks += `\n 𓍢ִ໋💜⊹ 𝗩𝗜𝗢𝗟𝗘𝗧 𝗕𝗢𝗧  𓍢ִ໋💜⊹`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
